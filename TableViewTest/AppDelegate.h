a047b4cecd0422cbac44f4d2707146450f2c8d20
SNCFDev <sncfdev@iMac-de-SNCFDev.local>
2017-11-24 14:10:20 +0100

    Initial Commit

---
+++ TableViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewTest
+//
+//  Created by SNCFDev on 24/11/2017.
+//  Copyright © 2017 SNCFDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
